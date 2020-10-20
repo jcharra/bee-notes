b322116b4f126de77c61affb10165ea733fbfde5
Johannes Charra <johannes.charra@gmx.de>
2020-10-20 21:43:27 +0200

    add reminder service with dialogs

---
+++ src/app/reminder.service.ts
@@ -0,0 +1,69 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { take, switchMap, map } from 'rxjs/operators';
+import { AuthService } from './auth/auth.service';
+
+export interface Reminder {
+  id?: string;
+  date: Date;
+  text: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReminderService {
+
+  constructor(private http: HttpClient,
+              private authService: AuthService) { }
+
+  getReminders(swarmId: string) {
+    return this.authService.user.pipe(
+      take(1),
+      switchMap((user) => {
+        if (!user) {
+          throw new Error('No user found');
+        }
+
+        return this.http
+          .get<{ [key: string]: any }>(
+            `https://beetracker-6865b.firebaseio.com/users/${user.id}/reminders/${swarmId}/reminders.json?auth=${user.token}`
+          )
+          .pipe(map(reminders => {
+
+            const entries: Reminder[] = [];
+            for (const key in reminders) {
+              if (reminders.hasOwnProperty(key)) {
+                entries.push({
+                  id: key,
+                  text: reminders[key].text,
+                  date: new Date(reminders[key].date)
+                });
+              }
+            }
+
+            return entries;
+          }));
+      }));
+  }
+
+  createReminder(swarmId: string, reminder: Reminder) {
+    return this.authService.user.pipe(
+      take(1),
+      switchMap((user) => {
+        if (!user) {
+          throw new Error('No user found');
+        }
+
+        return this.http
+          .post(
+            `https://beetracker-6865b.firebaseio.com/users/${user.id}/reminders/${swarmId}/reminders.json?auth=${user.token}`,
+            reminder
+          );
+      }));
+  }
+
+  deleteReminder(swarmId: string, reminderId: string) {
+
+  }
+}
--- src/app/swarms/swarm-detail/swarm-detail.page.ts
+++ src/app/swarms/swarm-detail/swarm-detail.page.ts
@@ -1,9 +1,12 @@
+import { ReminderService } from './../../reminder.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { LoadingController } from '@ionic/angular';
+import { LoadingController, AlertController, ToastController } from '@ionic/angular';
 import { JournalEntry } from 'src/app/journal.service';
 import { JournalService } from '../../journal.service';
 import { Swarm, SwarmService } from '../../swarm.service';
+import { NodeWithI18n } from '@angular/compiler';
+import { addDays, format, startOfDay } from 'date-fns';
 
 const JOURNAL_PLACEHOLDER = Array(3).fill({ text: '', date: new Date() });
 
@@ -22,7 +25,10 @@ export class SwarmDetailPage implements OnInit {
     private swarmService: SwarmService,
     private journalService: JournalService,
     private route: ActivatedRoute,
-    private loadingCtrl: LoadingController
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController,
+    private reminderService: ReminderService,
+    private toastController: ToastController
   ) { }
 
   ngOnInit() {
@@ -54,5 +60,72 @@ export class SwarmDetailPage implements OnInit {
         this.journalEntries = entries;
         withSpinner && loading.dismiss();
       });
+  }
+
+  async addReminder() {
+    const alert = await this.alertCtrl.create({
+      header: 'Add reminder',
+      inputs: [
+        {
+          name: 'text',
+          type: 'text',
+          placeholder: 'Reminder text'
+        }, {
+          name: 'days',
+          type: 'number',
+          placeholder: 'Number of days'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary'
+        },
+        {
+          text: 'Create reminder',
+          cssClass: 'primary',
+          handler: value => {
+            const text = value.text.trim();
+            const days = parseInt(value.days.trim());
+            if (!text || !days) {
+              this.onMissingValues();
+            } else {
+              const date = startOfDay(addDays(new Date, days));
+              const reminder = {
+                text,
+                date
+              };
+
+              this.reminderService
+              .createReminder(this.swarmId, reminder)
+              .subscribe(() => {
+                this.onReminderSaved(date);
+              });
+            }            
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async onMissingValues() {
+    const alert = await this.alertCtrl.create({
+      header: 'Error',
+      message: 'Invalid input. Please enter a description and an integer.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async onReminderSaved(date: Date) {
+    const toast = await this.toastController.create({
+      message: 'You will be reminded on ' + format(date, 'yyyy-MM-dd'),
+      duration: 2000
+    });
+    toast.present();
   }
 }
