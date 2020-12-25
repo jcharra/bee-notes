37c594082fb1a7332b79775398a1825059ff9e3f
jcharra <johannes.charra@gmx.de>
2020-12-25 23:47:51 +0100

    implement crappy drag & drop

--- src/app/swarms/swarms.page.ts
+++ src/app/swarms/swarms.page.ts
@@ -6,6 +6,8 @@ import {
   NavController,
 } from "@ionic/angular";
 import { ItemReorderEventDetail } from "@ionic/core";
+import { formatRelativeWithOptions } from "date-fns/fp";
+import { from } from "rxjs";
 import { first, tap } from "rxjs/operators";
 import { JournalEntry, JournalService } from "../journal.service";
 import { StatusService } from "../status.service";
@@ -257,14 +259,104 @@ export class SwarmsPage {
   }
 
   doReorder(ev: CustomEvent<ItemReorderEventDetail>) {
-    // The `from` and `to` properties contain the index of the item
-    // when the drag started and ended, respectively
     console.log("Dragged from index", ev.detail.from, "to", ev.detail.to);
 
-    // Finish the reorder and position the item in the DOM based on
-    // where the gesture ended. This method can also be called directly
-    // by the reorder group
+    let flatItems = [];
+
+    this.sortedSwarmGroups.forEach((g) => {
+      flatItems.push(g);
+      g.swarms.forEach((s) => {
+        flatItems.push(s);
+      });
+    });
+
+    const fromIdx = ev.detail.from;
+    const toIdx = ev.detail.to;
+    console.log(
+      "Moved item is ",
+      flatItems[fromIdx],
+      " taking place of ",
+      flatItems[toIdx]
+    );
+
+    const draggedItem = flatItems[fromIdx];
+
+    let fromGroup;
+    for (let i = fromIdx; i >= 0; i--) {
+      if (flatItems[i].swarms) {
+        fromGroup = flatItems[i];
+        break;
+      }
+    }
+
+    let toGroup;
+    for (let i = toIdx - (fromIdx > toIdx ? 1 : 0); i >= 0; i--) {
+      if (flatItems[i].swarms) {
+        toGroup = flatItems[i];
+        break;
+      }
+    }
+
+    console.log(
+      "Moved from group with name ",
+      fromGroup.name,
+      " to group ",
+      toGroup.name
+    );
+
+    if (fromGroup.id !== toGroup.id) {
+      this.sortedSwarmGroups.forEach((g) => {
+        if (g.id === fromGroup.id) {
+          g.swarms = g.swarms.filter((s) => s.id !== draggedItem.id);
+
+          if (g.id !== UNASSIGNED_GROUP) {
+            this.swarmGroupService
+              .updateGroup({
+                id: g.id,
+                name: g.name,
+                swarmIds: g.swarms.map((s) => s.id),
+              })
+              .subscribe();
+          }
+        } else if (g.id === toGroup.id) {
+          g.swarms.push(draggedItem);
+
+          if (g.id !== UNASSIGNED_GROUP) {
+            this.swarmGroupService
+              .updateGroup({
+                id: g.id,
+                name: g.name,
+                swarmIds: g.swarms.map((s) => s.id),
+              })
+              .subscribe(() => {
+                this.loadSwarms();
+              });
+          }
+        }
+      });
+    }
+
+    // Iterate forward and backward to get all items in group
+    const itemsInFinalGroup = [];
+    for (let f = toIdx; f > 0; f--) {
+      if (flatItems[f].swarms) {
+        break;
+      }
+      itemsInFinalGroup.push(flatItems[f]);
+    }
+
+    for (let t = toIdx; t < flatItems.length; t++) {
+      if (flatItems[t].swarms) {
+        break;
+      }
+      itemsInFinalGroup.push(flatItems[t]);
+    }
+
+    console.log("Found ", itemsInFinalGroup.length);
+
     ev.detail.complete();
+
+    this.loadSwarms();
   }
 
   _sortByIndex(a: Swarm, b: Swarm) {
