8a31ac09298d3759e770ef9b59a991f6d7457977
jcharra <johannes.charra@gmx.de>
2020-12-28 17:14:23 +0100

    send registration link only once

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { first, tap } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { Router } from "@angular/router";
+import { Observable } from "rxjs";
+import { first, tap } from "rxjs/operators";
 
 export interface AuthResponseData {
   kind: string;
@@ -15,43 +15,40 @@ export interface AuthResponseData {
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthService {
-  constructor(private auth: AngularFireAuth,
-              private router: Router) { }
+  constructor(private auth: AngularFireAuth, private router: Router) {}
 
   getUser(): Observable<firebase.User> {
-    return this.auth
-      .user
-      .pipe(
-        first(),
-        tap(u => { 
-          if (!u) {
-            this.router.navigateByUrl('/auth');
-            throw new Error('Unauthorized');
-          }
-          return u;
-      }));
+    return this.auth.user.pipe(
+      first(),
+      tap((u) => {
+        if (!u) {
+          this.router.navigateByUrl("/auth");
+          throw new Error("Unauthorized");
+        }
+        return u;
+      })
+    );
   }
-  
+
   signup(email: string, password: string) {
     return this.auth
       .createUserWithEmailAndPassword(email, password)
       .then(() => {
-        this.auth.user
-          .subscribe(user => {
-            return user.sendEmailVerification();
-          })
+        this.auth.user.pipe(first()).subscribe((user) => {
+          return user.sendEmailVerification();
+        });
       });
   }
 
   login(email: string, password: string) {
     return this.auth
       .signInWithEmailAndPassword(email, password)
-      .then((userCreds: firebase.auth.UserCredential) => { 
+      .then((userCreds: firebase.auth.UserCredential) => {
         if (!userCreds.user || !userCreds.user.emailVerified) {
-          throw 'User not yet verified';
+          throw "User not yet verified";
         }
       });
   }
