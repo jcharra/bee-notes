9bc923a930c07a98023d81798075a8882597ebc8
jcharra <johannes.charra@gmx.de>
2021-01-02 22:42:29 +0100

    more i18n

--- src/app/swarms/swarm-detail/swarm-detail.module.ts
+++ src/app/swarms/swarm-detail/swarm-detail.module.ts
@@ -1,13 +1,14 @@
-import { SharedModule } from '../../shared/shared.module';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { SharedModule } from "../../shared/shared.module";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from "@ionic/angular";
 
-import { SwarmDetailPageRoutingModule } from './swarm-detail-routing.module';
+import { SwarmDetailPageRoutingModule } from "./swarm-detail-routing.module";
 
-import { SwarmDetailPage } from './swarm-detail.page';
+import { SwarmDetailPage } from "./swarm-detail.page";
+import { TranslateModule } from "@ngx-translate/core";
 
 @NgModule({
   imports: [
@@ -15,8 +16,9 @@ import { SwarmDetailPage } from './swarm-detail.page';
     FormsModule,
     IonicModule,
     SharedModule,
-    SwarmDetailPageRoutingModule
+    SwarmDetailPageRoutingModule,
+    TranslateModule,
   ],
-  declarations: [SwarmDetailPage]
+  declarations: [SwarmDetailPage],
 })
 export class SwarmDetailPageModule {}
