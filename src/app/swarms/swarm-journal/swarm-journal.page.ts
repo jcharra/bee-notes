4c1686d39fad83fae20aafa9887a8ba747d7f153
jcharra <johannes.charra@gmx.de>
2021-01-20 10:07:35 +0100

    add journal filter

--- src/app/swarms/swarm-journal/swarm-journal.page.ts
+++ src/app/swarms/swarm-journal/swarm-journal.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { AlertController } from "@ionic/angular";
 import { format, getYear } from "date-fns";
-import { JournalService } from "src/app/journal.service";
+import { actionsForType, JournalService } from "src/app/journal.service";
 import { JournalEntry } from "./../../journal.service";
 
 @Component({
@@ -12,11 +12,13 @@ import { JournalEntry } from "./../../journal.service";
 })
 export class SwarmJournalPage implements OnInit {
   journalEntries: JournalEntry[];
+  filteredJournalEntries: JournalEntry[];
   swarmId: string;
   readonly: boolean;
   displayYear = getYear(new Date());
   maxYear = getYear(new Date());
   minYear = 2020;
+  filter: string = null;
 
   constructor(
     private journalService: JournalService,
@@ -40,7 +42,18 @@ export class SwarmJournalPage implements OnInit {
       .getEntries(this.swarmId, { startAt: startOfYear, endAt: endOfYear })
       .subscribe((es: JournalEntry[]) => {
         this.journalEntries = es || [];
+        this.updateFilteredEntries();
       });
+  }
+
+  updateFilteredEntries() {
+    if (!this.filter) {
+      this.filteredJournalEntries = this.journalEntries;
+    } else {
+      this.filteredJournalEntries = this.journalEntries.filter(
+        (e) => actionsForType[this.filter].indexOf(e.type) > -1
+      );
+    }
   }
 
   async deleteEntry(entry: JournalEntry) {
@@ -84,5 +97,14 @@ export class SwarmJournalPage implements OnInit {
       this.displayYear += amount;
       this.loadEntries();
     }
+  }
+
+  toggleFilter(tappedFilter: string) {
+    if (this.filter === tappedFilter) {
+      this.filter = null;
+    } else {
+      this.filter = tappedFilter;
+    }
+    this.updateFilteredEntries();
   }
 }
