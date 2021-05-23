65275890faa02639b09201ec0a9d134382a00f32
jcharra <johannes.charra@gmx.de>
2021-05-23 22:35:47 +0200

    swarm creation page

--- src/app/pages/swarms/swarm-edit/swarm-edit.page.ts
+++ src/app/pages/swarms/swarm-edit/swarm-edit.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { ActivatedRoute, Router } from "@angular/router";
 import { TranslateService } from "@ngx-translate/core";
+import { addYears, getYear } from "date-fns";
+import { Race } from "src/app/services/queen.service";
 import { SwarmService } from "src/app/services/swarm.service";
 
 @Component({
@@ -10,16 +13,37 @@ import { SwarmService } from "src/app/services/swarm.service";
 })
 export class SwarmEditPage implements OnInit {
   swarmId: string;
+  colonyForm: FormGroup;
+  races = [];
+  maxYear = getYear(new Date());
+  minYear = getYear(addYears(new Date(), -4));
 
   constructor(
     private swarmService: SwarmService,
     private route: ActivatedRoute,
     private router: Router,
-    private translate: TranslateService
-  ) {}
+    private translate: TranslateService,
+    private formBuilder: FormBuilder
+  ) {
+    this.races = Object.keys(Race);
+  }
 
   ngOnInit() {
-    console.log("Edit");
     this.swarmId = this.route.snapshot.params.swarmId;
+
+    this.colonyForm = this.formBuilder.group({
+      name: [null, Validators.required],
+      race: [Race.UNKNOWN],
+      birthYear: [getYear(new Date())],
+      isLayer: [false],
+      ancestor: [null],
+    });
+  }
+
+  changeBirthYear(diff: number) {
+    const newVal = this.colonyForm.controls.birthYear.value + diff;
+    if (newVal <= this.maxYear && newVal >= this.minYear) {
+      this.colonyForm.controls.birthYear.setValue(newVal);
+    }
   }
 }
