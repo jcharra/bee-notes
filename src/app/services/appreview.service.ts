4fdde3c78334807e2b9e7d953d8cbe13c3bf35f8
Johannes Charra <johannes.charra@gmail.com>
2021-07-15 23:03:10 +0200

    fix permanent review bug

--- src/app/services/appreview.service.ts
+++ src/app/services/appreview.service.ts
@@ -3,16 +3,17 @@ import { Swarm } from "../types/Swarm";
 import { RateApp } from "capacitor-rate-app";
 import { differenceInMonths } from "date-fns";
 import { Platform } from "@ionic/angular";
+import { Storage } from "@capacitor/storage";
 
 const validDate = (d) => d && d.getTime && !isNaN(d.getTime());
-
+const storageLastReviewRequestDate = "LAST_REVIEW_REQUEST";
 @Injectable({
   providedIn: "root",
 })
 export class AppreviewService {
   constructor(private plt: Platform) {}
 
-  checkReview(swarms: Swarm[]) {
+  async checkReview(swarms: Swarm[]) {
     if (!this.plt.is("ios")) {
       return;
     }
@@ -21,22 +22,37 @@ export class AppreviewService {
       return;
     }
 
-    let createdDates = swarms.map((s) => s.created).filter(validDate);
+    const lastDate = await Storage.get({ key: storageLastReviewRequestDate });
+    if (lastDate && lastDate.value) {
+      if (differenceInMonths(new Date(), new Date(lastDate.value)) > 3) {
+        this.requestReview();
+      }
+    } else {
+      // The user has never been asked for a review => determine how long ago
+      // the creation of the first colony has been
+      let createdDates = swarms.map((s) => s.created).filter(validDate);
 
-    if (createdDates.length === 0) {
-      return;
+      if (createdDates.length === 0) {
+        return;
+      }
+
+      createdDates.sort();
+
+      const oldest = createdDates[0];
+
+      const monthsAgo = differenceInMonths(new Date(), oldest);
+
+      if (monthsAgo > 2) {
+        this.requestReview();
+      }
     }
+  }
 
-    createdDates.sort();
-
-    const oldest = createdDates[0];
-
-    const monthsAgo = differenceInMonths(new Date(), oldest);
-
-    // If the oldest colony was created 1, 4, 7, 10, ... months ago,
-    // ask for a nice review.
-    if (monthsAgo % 3 === 1) {
-      RateApp.requestReview();
-    }
+  private async requestReview() {
+    await Storage.set({
+      key: storageLastReviewRequestDate,
+      value: new Date().toISOString(),
+    });
+    RateApp.requestReview();
   }
 }
