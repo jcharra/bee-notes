d097a836132bbd81cc78009118f4f4c6f78f31ed
Johannes Charra <johannes.charra@gmail.com>
2021-12-25 12:55:42 +0100

    include android files, upgrade some libs

---
+++ android/app/src/main/java/de/charra/beenotes/MainActivity.java
@@ -0,0 +1,12 @@
+package de.charra.beenotes;
+
+import android.os.Bundle;
+
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+
+import java.util.ArrayList;
+
+public class MainActivity extends BridgeActivity {
+
+}
